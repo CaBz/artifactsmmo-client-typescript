fd29855f5a2132d24195e6de4e3e12b07125a92b
Charles-Alexandre Blais <cabzzz@gmail.com>
2025-03-12 23:06:18 -0400

    random stuff for web

--- web/server.ts
+++ web/server.ts
@@ -1,23 +1,26 @@
 import express, { Request, Response } from 'express';
-import {Container} from "../src/Container";
+import {Container} from "../src/Container.js";
 
-const containerCdl = await Container.create('Richard_CDL');
+const container = await Container.create('Richard_CDL');
+const characters = await container.client.getAllCharacterStatus();
 
 const port = +(process.env.PORT || 3000);
 const app = express();
 
-app.use(express.static('web/assets'))
+app.use('/assets', express.static('web/assets'))
 app.set('view engine', 'ejs');
 app.set('views', 'web/views');
 
 app.get('/', async (req: Request, res: Response) => {
-    res.render('pages/index');
+    res.render('pages/index', { active: 'home', characters });
 });
 
 app.get('/bank', async (req: Request, res: Response) => {
-    const bank = await containerCdl.client.getBank();
+    const bank = await container.client.getBank();
+    bank.items.sort((a, b) => a.code.localeCompare(b.name));
+    bank.items = bank.items.map((item) => ({ value: container.items.get(item.code), quantity: item.quantity}));
 
-    res.render('pages/bank', { bank });
+    res.render('pages/bank', { active: 'bank', characters, bank });
 });
 
 app.listen(port, () => {
